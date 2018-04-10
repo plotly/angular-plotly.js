c96fdd74133fe0942cac1f08f80a492fb13767de
Andre Farzat <andrefarzat@gmail.com>
2018-04-10 02:09:49 -0300

    Creating the index.ts and public_api.ts

---
+++ public_api.ts
@@ -0,0 +1,3 @@
+export { PlotlyModule } from './src/app/plotly/plotly.module';
+export { PlotComponent } from './src/app/plotly/plot/plot.component';
+export { PlotlyService } from './src/app/plotly/plotly.service';
