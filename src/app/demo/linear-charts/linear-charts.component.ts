14ee5e6c3c211727b68a0896eed81592418ca0fe
Andre Farzat <andrefarzat@gmail.com>
2018-05-03 21:43:48 -0300

    Quickfix typings in linear-charts.component.ts

--- src/app/demo/linear-charts/linear-charts.component.ts
+++ src/app/demo/linear-charts/linear-charts.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import * as Plotly from '../../plotly/plotly';
 
 @Component({
     selector: 'plotly-line-charts',
@@ -9,18 +8,18 @@ export class LinearChartsComponent {
     public debug = true;
     public useResizeHandler = true;
 
-    public data: Plotly.Data[] = [
+    public data: any[] = [
         { x: [1, 2, 3, 4], y: [10, 15, 13, 17], mode: 'markers', name: 'Scatter' },
         { x: [2, 3, 4, 5], y: [16, 5, 11, 9], mode: 'lines', name: 'Lines' },
         { x: [1, 2, 3, 4], y: [12, 9, 15, 12], mode: 'lines+markers', name: 'Scatter + Lines' },
     ];
 
-    public layout: Partial<Plotly.Layout> = {
+    public layout: any = {
         title: 'Adding Names to Line and Scatter Plot',
     };
 
     public addRandomLine() {
-        const line: Plotly.Data = { x: [], y: [], mode: 'lines', name: 'Line ' + this.data.length };
+        const line: any = { x: [], y: [], mode: 'lines', name: 'Line ' + this.data.length };
 
         line.x = [1, 2, 3, 4].map(i => Math.round(Math.random() * 10));
         line.y = [1, 2, 3, 4].map(i => Math.round(Math.random() * 20));
