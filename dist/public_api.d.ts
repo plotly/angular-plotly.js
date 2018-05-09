156ff58ef187267d2c441f7842e572510b06653a
Andre Farzat <andrefarzat@gmail.com>
2018-05-09 14:56:21 -0300

    Removing and ignoring dist folder

--- dist/public_api.d.ts
+++
@@ -1,3 +0,0 @@
-export { PlotlyModule } from './src/app/plotly/plotly.module';
-export { PlotComponent } from './src/app/plotly/plot/plot.component';
-export { PlotlyService } from './src/app/plotly/plotly.service';
