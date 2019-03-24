e98bc68de31f78671b517cc6625992f0c131281e
André Farzat <andrefarzat@gmail.com>
2019-03-24 20:14:58 -0300

    Adding tests for plotly.module

---
+++ src/app/plotly/plotly.module.spec.ts
@@ -0,0 +1,25 @@
+import { PlotlyModule } from "./plotly.module";
+
+
+
+describe('PlotlyModule', () => {
+
+    it('should create', () => {
+        const fn = () => {
+            const mod = new PlotlyModule();
+        };
+
+        const msg = "Invalid PlotlyJS object. Please check https://github.com/plotly/angular-plotly.js#quick-start"
+                  + " to see how to add PlotlyJS to your project.";
+        expect(fn).toThrowError(msg);
+
+
+        const fn2 = () => {
+            PlotlyModule.plotlyjs = {plot: function() { }};
+            const mod = new PlotlyModule();
+        };
+
+        expect(fn2).not.toThrowError(msg);
+    });
+
+});
