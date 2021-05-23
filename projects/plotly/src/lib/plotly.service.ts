032a24c0e383c697579f05003a62962336436e01
xaxis-ii <simonkoch96@gmail.com>
2021-05-24 00:26:58 +0200

    Update to support Plotly 2.0.0 interface.

--- projects/plotly/src/lib/plotly.service.ts
+++ projects/plotly/src/lib/plotly.service.ts
@@ -95,10 +95,20 @@ export class PlotlyService {
     public plot(div: Plotly.PlotlyHTMLElement, data: Plotly.Data[], layout?: Partial<Plotly.Layout>, config?: Partial<Plotly.Config>, frames?: Partial<Plotly.Config>[]): Promise<any>  {
         if (frames) {
             const obj = {data, layout, config, frames};
-            return this._getPlotly().plot(div, obj) as Promise<any>;
+            if (typeOf(this.__getPlotly.plot === 'function')) {
+                return this._getPlotly().plot(div, obj) as Promise<any>;
+            } else {
+                // Adds support for Plotly 2.0.0 release candidates
+                return this._getPlotly().newPlot(div, obj) as Promise<any>;
+            }
         }
 
-        return this._getPlotly().plot(div, data, layout, config) as Promise<any>;
+        if (typeOf(this.__getPlotly.plot === 'function')) {
+            return this._getPlotly().plot(div, data, layout, config) as Promise<any>;
+        } else {
+            // Adds support for Plotly 2.0.0 release candidates
+            return this._getPlotly().newPlot(div, data, layout, config) as Promise<any>;
+        }
     }
 
     public update(div: Plotly.PlotlyHTMLElement, data: Plotly.Data[], layout?: Partial<Plotly.Layout>, config?: Partial<Plotly.Config>, frames?: Partial<Plotly.Config>[]): Promise<any>  {
