225533e9e5bd58b4de7322a597f74602ed6634f5
Andre Farzat <andrefarzat@gmail.com>
2021-08-15 14:58:32 -0300

    Removing index.d.ts file

--- projects/plotly/src/index.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'plotly.js-dist-min';
--- projects/plotly/src/public-api.ts
+++ projects/plotly/src/public-api.ts
@@ -5,6 +5,7 @@
 export * from './lib/plotly.module';
 export * from './lib/plotly-via-cdn.module';
 export * from './lib/plotly-via-window.module';
+export * from './lib/plotly-shared.module';
 
 export * from './lib/plotly.service';
 export * from './lib/plotly.component';
