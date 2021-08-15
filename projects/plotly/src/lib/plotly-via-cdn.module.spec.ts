f4c3f935cdb93c31b6db8d8f57976282a32b4c99
Andre Farzat <andrefarzat@gmail.com>
2021-08-15 16:00:56 -0300

    Fixing tests in several files

---
+++ projects/plotly/src/lib/plotly-via-cdn.module.spec.ts
@@ -0,0 +1,36 @@
+import { PlotlyBundleName } from 'dist/plotly/public-api';
+
+import { PlotlyViaCDNModule } from './plotly-via-cdn.module';
+import { PlotlyService } from './plotly.service';
+
+describe('PlotlyViaCDNModule', () => {
+    let plotlyServiceCache: any = {};
+
+    beforeEach(() => {
+        plotlyServiceCache.moduleName = (PlotlyService as any).moduleName;
+    });
+
+    afterEach(() => {
+        (PlotlyService as any).moduleName = plotlyServiceCache.moduleName;
+    });
+
+    it('should create', () => {
+        const plotlyService = jasmine.createSpyObj('PlotlyService', ['setModuleName']);
+        const _ = new PlotlyViaCDNModule(plotlyService);
+
+        expect((PlotlyService as any).moduleName).toBe('ViaCDN');
+    });
+
+    it('should set Plotly Bundle', () => {
+        const validValues: any[] = ['basic', 'cartesian', 'geo', 'gl3d', 'gl2d', 'mapbox', 'finance', null];
+        for (const value of validValues) {
+            PlotlyViaCDNModule.setPlotlyBundle(value);
+            expect((PlotlyViaCDNModule as any).plotlyBundle).toBe(value);
+        }
+
+        const fn = () => (PlotlyViaCDNModule as any).setPlotlyBundle('Invalid name');
+        const msg = `Invalid plotly bundle. Please set to null for full or "basic", "cartesian", "geo", "gl3d", "gl2d", "mapbox", "finance" for a partial bundle.`;
+        expect(fn).toThrowError(msg);
+    });
+
+});
